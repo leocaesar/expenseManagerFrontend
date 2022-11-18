f365db9e9b62a734a5822668c786c31bc2448b1d
Victoria Luna Terang Lotussa <U071374@bca.co.id>
2022-11-18 10:25:18 +0700

    LUN income list,edit,delete

--- src/app/income-edit/income-edit.component.ts
+++ src/app/income-edit/income-edit.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Income, IncomeModel } from '../model/income';
+import { IncomeService } from '../service/income.service';
+import { Location } from '@angular/common';
+
 
 @Component({
   selector: 'app-income-edit',
@@ -7,9 +12,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IncomeEditComponent implements OnInit {
 
-  constructor() { }
+  incomes: Income = {
+    id: 0,
+    date: "",
+    description:"",
+    amount:0,
+    category_income:0
+  };
+  income2: Income[] = [];
+  title: string = "Edit INCOME"
+  count: number = 1;
+  inc(value: number) {
+    this.count = this.count++;
+  }
+
+  modelIncome = new IncomeModel('','',0,0)
+  submitted = false;
+
+  constructor(private incomeService: IncomeService, private location: Location,
+    private route: ActivatedRoute, private router: Router) {
+  }
 
   ngOnInit(): void {
+    //console.log(this.route?.snapshot.params['id'])
+    this.incomeService.getIncome(this.route?.snapshot.params['id']).subscribe(
+      (result) => {
+        this.incomes = result
+        console.log(this.incomes)
+      }
+    )
+    this.getIncomes2()
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    this.updateIncomes()
+  }
+  validateNo(e: any): boolean {
+    const charCode = e.which ? e.which : e.keyCode;
+    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+      return false
+    }
+    return true
+  }
+  updateIncomes(): void {
+    // console.log(this.freelancer)
+    this.incomeService.editIncomesService(this.incomes.id,this.incomes)
+      .subscribe(incomeCategories => {
+        alert("SUKSES")
+      })
+  }
+  getIncomes2(): void {
+    this.incomeService.getIncomes().subscribe(incomes => this.income2 = incomes);
   }
 
 }
--- src/app/income-list/income-list.component.ts
+++ src/app/income-list/income-list.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Router } from '@angular/router';
+import { IncomeComponent } from '../income/income.component';
+import { Income } from '../model/income'
 
 @Component({
   selector: 'app-income-list',
   templateUrl: './income-list.component.html',
   styleUrls: ['./income-list.component.css']
 })
-export class IncomeListComponent implements OnInit {
+export class IncomeListComponent {
+  newItem = "TESTING FREELANCE";
 
-  constructor() { }
-
-  ngOnInit(): void {
+  @Input() income!: Income;
+  @Output() addIncomeEvent = new EventEmitter<Income>();
+  addNewIncome(income: Income) {
+    this.addIncomeEvent.emit(income);
   }
+  constructor(private incomes: IncomeComponent, private router: Router) { }
+  ngOnInit(): void { }
+
+  sendDelete(id: number): void {
+    this.incomes.onDelete(id);
+  }
+
+  sendEdit(id: number): void {
+    this.router.navigateByUrl("/income-edit/"+id)
+  }
+
 
 }
